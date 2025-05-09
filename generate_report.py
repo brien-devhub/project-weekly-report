d5a57e8815d4b032d621dd84b9f22a0e143803b2
brien-devhub <brien@devhub.com>
2025-05-09 11:26:40 -0400

    CHANGED IDS

--- generate_report.py
+++ generate_report.py
@@ -1,8 +1,8 @@
 import os, requests
 
 # pull your PAT and Report GID from environment
-PAT = os.environ['2/1207622752023949/1210162423953290:199fcbcb526f1b4e40b76f95aa658dd9']
-REPORT_GID = os.environ['1210155607259625']
+PAT = os.environ['ASANA_PAT']
+REPORT_GID = os.environ['ASANA_REPORT_GID']
 
 url = f'https://app.asana.com/api/1.0/reports/{REPORT_GID}/results'
 resp = requests.get(url, headers={'Authorization': f'Bearer {PAT}'})
